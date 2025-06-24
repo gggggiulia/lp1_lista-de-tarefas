50279e82a45b8406681bf2c69cda8ddf78a1913a
giulia <169063222+gggggiulia@users.noreply.github.com>
2025-06-24 09:04:04 -0300

    Update Tarefa.java

--- src/Tarefa.java
+++ src/Tarefa.java
@@ -1,1 +1,35 @@
+package br.edu.ifsp.spo.java.todolist;
 
+public class Tarefa {
+    private String texto;
+    private boolean concluido;
+    private LocalDateTime dataConclusao;
+    
+    //Construtor
+    public Tarefa(String descricao) {
+        this.descricao = descricao;
+        this.concluido = false;
+        this.dataConclusao = null;
+    }
+
+    public String getTexto() {
+        return texto;
+    }
+
+    public boolean isConcluido() {
+        return check;
+    }
+
+    public LocalDateTime getDataConclusao() {
+        return dataConclusao;
+    }
+
+    public void check() {
+        check = true;
+    }
+
+    public void uncheck() {
+        check = false;
+        dataConclusao = null;
+    }
+}
